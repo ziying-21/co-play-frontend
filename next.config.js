8b19b4519cfadb627d04bc82694df7631a7b068c
张子颖 <790772462@qq.com>
2023-12-29 15:54:02 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: { unoptimized: true },
+
+  async rewrites() {
+    return [{
+      source: "/api/:path*",
+      destination: "http://127.0.0.1:8000/:path*",
+    }];
+  }
 }
 
 module.exports = nextConfig
